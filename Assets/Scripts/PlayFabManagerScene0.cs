1b070d810dfd99c99f15679363b44dad94dd68cd
Ted Bonga <tedbonga4.tb@gmail.com>
2022-06-09 20:55:48 +0200

    Add credit menu in the Main menu

--- Assets/Scripts/PlayFabManagerScene0.cs
+++ Assets/Scripts/PlayFabManagerScene0.cs
@@ -6,22 +6,45 @@ using PlayFab.ClientModels;
 using System;
 using Newtonsoft.Json;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayFabManagerScene0 : MonoBehaviour
 {
     [Header("UI")]
     public Text messageText;
+    [SerializeField] GameObject message;
     public InputField usernameInput;
     public InputField emailInput;
     public InputField passwordInput;
-    public GameObject mainMenuPanel;
-    public GameObject loginPanel;
+    [SerializeField] GameObject registerPanel;
+    [SerializeField] GameObject loginPanel;
+    [SerializeField] GameObject confirmPanel;
     //Start is called before the first frame update
     void Start()
     {
 
     }
+    public void OpenRegisterMenu()
+    {
+        loginPanel.SetActive(false);
+        registerPanel.SetActive(true);
+        message.SetActive(false);
+    }
 
+    public void GoBack()
+    {
+        loginPanel.SetActive(true);
+        registerPanel.SetActive(false);
+        confirmPanel.SetActive(false);
+        message.SetActive(false);
+    }
+    public void OpenConfirmMenu()
+    {
+        loginPanel.SetActive(false);
+        registerPanel.SetActive(false);
+        confirmPanel.SetActive(true);
+        message.SetActive(false);
+    }
 
     public void RegisterButton()
     {
@@ -37,12 +60,16 @@ public class PlayFabManagerScene0 : MonoBehaviour
             Password = passwordInput.text,
             RequireBothUsernameAndEmail = false
         };
-        PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnError);
+        PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnErrorRegister);
     }
 
     void OnRegisterSuccess(RegisterPlayFabUserResult result)
     {
         messageText.text = "Registered and logged in!";
+        loginPanel.SetActive(false);
+        registerPanel.SetActive(false);
+        confirmPanel.SetActive(true);
+        AddOrUpdateContactEmail(emailInput.text);
     }
 
     public void LoginButton()
@@ -51,44 +78,92 @@ public class PlayFabManagerScene0 : MonoBehaviour
             Email=emailInput.text,
             Password=passwordInput.text
         };
-        PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnError);
+        PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnErrorLogin);
     }
 
     private void OnLoginSuccess(LoginResult result)
     {
+        message.SetActive(true);
         messageText.text = "Login success";
         Debug.Log("Login success");
-        mainMenuPanel.SetActive(true);
-        loginPanel.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        SceneManager.LoadScene("menuScene");
         
+
     }
 
-    void Login()
-    {
-        var request = new LoginWithCustomIDRequest
-        {
-            CustomId = SystemInfo.deviceUniqueIdentifier,
-            CreateAccount = true
-        };
-        PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
-    }
+   // void Login()
+    //{
+      //  var request = new LoginWithCustomIDRequest
+        //{
+          //  CustomId = SystemInfo.deviceUniqueIdentifier,
+            //CreateAccount = true
+        //};
+        //PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
+    //}
+
+
     void OnSuccess(LoginResult result)
     {
         Debug.Log("Success Login");
     }
 
 
-    void OnError(PlayFabError error)
+    void OnErrorRegister(PlayFabError error)
     {
+        message.SetActive(true);
         Debug.Log("Error");
-        messageText.text = error.ErrorMessage;
+        messageText.text = "Register Not Succesful";
     }
+    void OnErrorLogin(PlayFabError error)
+    {
+        message.SetActive(true);
+        Debug.Log("Error");
+        messageText.text = "Login not successful!  Try Again!";
+    }
+    /*
+    void AddContactEmailToPlayer()
+    {
+       var loginReq = new LoginWithCustomIDRequest
+        {
+            CustomId = SystemInfo.deviceUniqueIdentifier, // replace with your own Custom ID
+            CreateAccount = true // otherwise this will create an account with that ID
+        }; 
+        var request= new LoginWithEmailAddressRequest
+        {
+            Email = emailInput.text,
+            Password = passwordInput.text
+        };
+        PlayFabClientAPI.LoginWithEmailAddress(request, loginRes =>
+        {
+            Debug.Log("Successfully logged in player with PlayFabId: " + loginRes.PlayFabId);
+            AddOrUpdateContactEmail(emailInput.text);
+        }, OnError);
+    } 
+    
+    var emailAddress = emailInput.text; // Set this to your own email for testing
 
+    PlayFabClientAPI.LoginWithCustomID(loginReq, loginRes =>
+        {
+            Debug.Log("Successfully logged in player with PlayFabId: " + loginRes.PlayFabId),
+            AddOrUpdateContactEmail(emailAddress);
+        }, FailureCallback);  */
 
-  
+    void AddOrUpdateContactEmail(string emailAddress) {
+    var request = new AddOrUpdateContactEmailRequest
+    {
+        EmailAddress = emailAddress
+    };
+    PlayFabClientAPI.AddOrUpdateContactEmail(request, result =>
+    {
+        Debug.Log("The player's account has been updated with a contact email");
+    }, FailureCallback);
+}
+
+    void FailureCallback(PlayFabError error)
+    {
+        Debug.LogWarning("Something went wrong with your API call. Here's some debug information:");
+        Debug.LogError(error.GenerateErrorReport());
+    }
+    
+
 }
